7b6d45bd32cf432799c470d86807caf60b1607c1
longhoang <long542.nt@gmail.com>
2025-07-17 10:39:25 +0700

    bai75

--- src/saga/counter.saga.ts
+++ src/saga/counter.saga.ts
@@ -1,23 +1,26 @@
 import { put, takeEvery } from "redux-saga/effects"
+import { decreaseSagaFinish, increaseSagaFinish } from "../redux/counter/counter.slide";
 
 function* handleSage(action: any){
     console.log(">>> check handleSage: ",action)
 }
 function* handleIncrease(action: any){
     console.log(">>>check handleSage: ",action);
-    yield(3000)
-    yield put({
-        type:"counter/increaseSagaFinish",
-        payload:{value:2}
-    })
+    // yield(3000)
+    // yield put({
+    //     type:"counter/increaseSagaFinish",
+    //     payload:{value:2}
+    // })
+    yield put(increaseSagaFinish({value:2}))
 }
 function* handleDecrease(action: any){
     console.log(">>>check handleSage: ",action);
-    yield(3000)
-    yield put({
-        type:"counter/decreaseSagaFinish",
-        payload:{value:2}
-    })
+    // yield(3000)
+    // yield put({
+    //     type:"counter/decreaseSagaFinish",
+    //     payload:{value:2}
+    // })
+    yield put(decreaseSagaFinish({value:2}))
 }
 function* counterSaga(){
     yield takeEvery("counter/increaseSagaStart",handleIncrease),
